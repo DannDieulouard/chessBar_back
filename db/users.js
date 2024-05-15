b5d12346b9abf10313799642159c2af05362e547
Dann Dieulouard <dann.dieulouard@lapiscine.pro>
2024-05-15 18:03:41 +0200

    adding new categories to example users

--- db/users.js
+++ db/users.js
@@ -1,26 +1,39 @@
 const users = [
     {
         id: 1,
-        username: "danndieulouard",
+        surname: "Dann",
+        name: "Dieulouard",
+        username: "Steelskin8",
+        city: "Merignac",
+        postCode: "33700",
         password: "5*^9^@HH58m#2wTf3az4",
-        address: { "number": "6", "street": "rue Victor Hugo", "postCode": 33000, "city": "Bordeaux" },
-        email: "dann.dieulouard@example.com",
+        email: "dann.dieulouard@gmail.com",
+        howChessbar: "Grâce à Kevin B.",
         RoleId: 1
     },
     {
         id: 2,
-        username: "mathieujalibert",
+        surname: "Kevin",
+        name: "Benichou",
+        username: "Spasskiki",
+        city: "Bordeaux",
+        postCode: "33000",
         password: "mdp",
-        address: { "number": "6", "street": "rue Victor Hugo", "postCode": 33000, "city": "Bordeaux" },
-        email: "mathieu.jalibert@example.com",
+        email: "spasskiki33@example.com",
+        howChessbar: "C'est moi le créateur en fait.",
         RoleId: 2
     },
     {
         id: 3,
-        username: "antoinedupont",
+        surname: "François",
+        name: "Gallice",
+        username: "Xoulph",
+        city: "Paris",
+        postCode: "75000",
         password: "mdp",
-        address: { "number": "61", "street": "rue Albert Camus", "postCode": 33000, "city": "Bordeaux" },
-        email: "antoine.dupont@example.com"
+        email: "xoulph33@example.com",
+        howChessbar: "Par des connaissances qui m'en ont parlé.",
+        RoleId: 3
     }
 ];
 
